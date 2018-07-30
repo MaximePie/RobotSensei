80e525bc6ab82f62c2fccdec58edf1babb5bd7d8
Maxime Pie <chamalowat@laposte.net>
2018-07-30 17:45:49 +0200

    Création d'une landing page

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class SurveyPage extends React.Component {
+    render(){
+        return(
+            <div>Bienvenue sur la landing Page</div>
+        )
+    }
+}
